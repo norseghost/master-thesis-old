5077be8ce6f700a8983a3696ff899eee72a193f8
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2020-03-06 06:26:54 +0100

    [code] første udkast af gofastr kode

---
+++ bin/ft_gofastr.R
@@ -0,0 +1,15 @@
+# ft_gofastr.R 
+# try out the gofstr package fro rapid creation of DTM / TDM
+
+library(here)
+library(gofastr)
+library(tm)
+library(tidyverse)
+library(data.table)
+
+# load cleaned dataset into menory
+ft_speeches <- fread(here("data/folketinget_cleaned.csv"))
+
+ft_tdm <- q_tdm(ft_speeches$text, ft_speeches$doc_id)
+
+ft_dtm <- q_dtm(ft_speeches$text, ft_speeches$doc_id)
