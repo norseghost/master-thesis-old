cf014d006bd8edc15e674d942b41ea5f835841c1
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2020-03-06 05:50:36 +0100

    [code] fix brainfart in corups cleaning function
    All hail the mighty magrittr! so much nicer than super-nested function
    calls.

--- bin/ft_cleanup.R
+++ bin/ft_cleanup.R
@@ -9,28 +9,36 @@ library(parallel)
 
 ft_speeches <- fread(here("data/folketinget_1953_2019_raw.csv"))
 
-cluster <- makePSOCKcluster(names = 4, outfile = "")
 
 # I'll probably want more stopwords later
 # after some exploratory analysis
 ft_stopwords <- c(stopwords("danish"))
 clean_text <- function(text) {
-    removeNumbers(text)
-    removePunctuation(text)
-    removeWords(text, ft_stopwords)
-    stripWhitespace(text)
+    text %>%
+    removeNumbers %>%
+    removePunctuation %>%
+    removeWords(., ft_stopwords) %>%
     # the corpus contains occurences of hangul character
     # hwalp - 홢 - this is unwanted
-    str_remove_all(text, '홢')
-    tolower(text)
+    str_remove_all(., "홢") %>%
+    stripWhitespace %>%
+    tolower
 }
 
+ft_speeches[, text := sapply(
+                             .SD[, text], clean_text),
+             by = .groups]
+# tried running this in parallel
+# but that killed my laptop :(
+# OOm maybe?
+ cluster <- makePSOCKcluster(names = 4, outfile = "")
 # the cluster needs to see my stopwords
 clusterExport(
         cl = cluster,
         varlist = c("ft_stopwords"),
         envir = .GlobalEnv
 )
+#and needs to have the required libraries loaded
 clusterEvalQ(
     cl = cluster, {
         library(tm)
@@ -43,3 +51,4 @@ clusterEvalQ(
 ft_speeches[, text := parSapply(
                             cluster, .SD[, text], clean_text),
             by = .groups]
+
