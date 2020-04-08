8fdf09bab8598596cec665c23dc118218f99a129
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2020-04-09 00:21:23 +0200

    [code] text cleanup script
    Includes mini-snippet for filtering out group 99. That should probably
    be a function, actually.

--- bin/ft_cleanup.R
+++ bin/ft_cleanup.R
@@ -5,21 +5,22 @@ library(tidyverse)
 library(data.table)
 library(tm)
 library(here)
-library(parallel)
+
 
 ft_raw <- fread(here("data/folketinget_1953_2019_raw.csv"))
+
 # prepare udpipe for lemmatization
 library(udpipe)
 udmodel <- udpipe_download_model("danish")
-lemmatize_speeches <- function(text) {
+lemmatize <- function(text) {
     dt <- udpipe_annotate(
         object = udpipe_load_model(udmodel$file_model),
         x = text,
-        doc_id = "1",
+        # tokenizer = "horizontal",
         tagger = "default",
         parser = "none",
         trace = 0
-    ) %>% as.data.table
+    ) %>% as.data.table 
     lemmata <- dt %>%
         select(lemma) %>%
         unlist %>%
@@ -27,41 +28,42 @@ lemmatize_speeches <- function(text) {
     return(lemmata)
 }
 
-test <- lemmatize_speeches(ft_raw$text[[355]], ft_raw$doc_id[[355]])
-
-
 # I'll probably want more stopwords later
 # after some exploratory analysis
+ft_stopwords <- c(
 
-ft_stopwords <- c(stopwords("danish"))
+    readLines(
+        "https://raw.githubusercontent.com/stopwords-iso/stopwords-da/master/stopwords-da.txt",
+        warn = FALSE),
+    readLines(
+        here("bin/stopwords.txt"))
+)
+
 clean_text <- function(text) {
     text %>%
+    tolower %>%
     removeNumbers %>%
     removePunctuation %>%
-    removeWords(., ft_stopwords) %>%
+    removeWords(stopwords) %>%
+    lemmatize %>%
     # the corpus contains occurences of hangul character
     # hwalp - 홢 - this is unwanted
     str_remove_all(., "홢") %>%
-    stripWhitespace %>%
-    tolower
+    stripWhitespace
 }
+gr99 <- ft_raw[.groups == 99] %>% as.data.table
 
-ft_clean <- ft_raw[, text := sapply(
-                             .SD[, text], clean_text),
-             by = .groups]
+gr99_clean <- gr99[, text := sapply(
+                            .SD[, text], clean_text),
+             by = .I]
 
-ft_lemmatized <- ft_clean[, text := sapply(
-                             .SD[, text], lemmatize_speeches),
-             by = .groups]
-
-# tried running this in parallel
-# but that killed my laptop :(
-# OOm maybe?
-cluster <- makePSOCKcluster(names = 4, outfile = "")
+# set up parallel processing
+library(parallel)
+cluster <- makeForkCluster(nnodes = 3, outfile = "")
 # the cluster needs to se e my stopwords
 clusterExport(
         cl = cluster,
-        varlist = c("ft_stopwords"),
+        varlist = c("ft_stopwords", "lemmatize", "clean_text"),
         envir = .GlobalEnv
 )
 #and needs to have the required libraries loaded
@@ -69,8 +71,12 @@ clusterEvalQ(
     cl = cluster, {
         library(tm)
         library(tidyverse)
+        library(udpipe)
     }
 )
+gr99_clean <- gr99[, text := parSapply(
+                            cluster, .SD[, text], clean_text),
+             by = .I]
 
 # apply the cleaning operation in parallel
 # the dataset is already prepared in 100 batches
@@ -78,3 +84,17 @@ ft_speeches[, text := parSapply(
                             cluster, .SD[, text], clean_text),
             by = .groups]
 
+
+# apply the cleaning operation in parallel
+# the dataset is already prepared in 100 batches
+ft_speeches[, text := parSapply(
+                            cluster, .SD[, text], clean_text),
+            by = .groups]
+
+ft_clean <- ft_raw[, text := sapply(
+                             .SD[, text], clean_text),
+             by = .groups]
+
+ft_lemmatized <- ft_clean[, text := sapply(
+                             .SD[, text], lemmatize),
+             by = .groups]
