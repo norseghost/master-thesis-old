9d03168f5442fc0c7222e8890a46a23e3071308a
Martin Andreas Andersen <martin.andreas.andersen@gmail.com>
2020-05-17 05:18:28 +0200

    code: use proper date math in period assignment
    Some observations have an empty Period field, which fucked my timeseries
    function but fierce :) proper date maath works better.

--- bin/ft_periods.R
+++ bin/ft_periods.R
@@ -10,27 +10,27 @@
 # 57-68 (udvidelse af lærlingekonceptet)
 # 68-78 (EFG)
 # 78-90 (Haarder, U91, EUD/EUX)
-# 90-00 (uddannelse til alle, markedsorientering, selvstyre)
-# 00-14 (individualisering, ansvar for egen læring)
+# 90-01 (uddannelse til alle, markedsorientering, selvstyre)
+# 01-14 (individualisering, ansvar for egen læring)
 # 14-20 (fokus på unge; voksne falder fra)
 
 library(here)
 library(data.table)
 library(tidyverse)
+library(future)
+
+options(future.globals.maxSize= 89128960000)
 
 ft_speeches <- fread(here("data/ft_clean_lemmatized.csv"))
-
-ft_speeches[, timeseries := sapply(Period, (function(x) {
-                            year <- as.integer(str_extract(x, "\\d{4}$"))
+ft_speeches[, timeseries := sapply(Date, (function(x) {
                             case_when(
-                                 year <= 1957 ~ "1953-57",
-                                 year <= 1968 ~ "1957-68",
-                                 year <= 1978 ~ "1968-78",
-                                 year <= 1990 ~ "1978-90",
-                                 year <= 2000 ~ "1990-00",
-                                 year <= 2014 ~ "2000-14",
-                                 year <= 2020 ~ "2014-20"
+                                 x < as.Date("1957-05-28") ~ "1953-57",
+                                 x < as.Date("1968-02-02") ~ "1957-68",
+                                 x < as.Date("1978-08-30") ~ "1968-78",
+                                 x < as.Date("1990-12-18") ~ "1978-90",
+                                 x < as.Date("2001-11-27") ~ "1990-01",
+                                 x < as.Date("2014-02-03") ~ "2001-14",
+                                 x > as.Date("2014-02-03") ~ "2014-20"
                             )}
                         ))]
-
 
